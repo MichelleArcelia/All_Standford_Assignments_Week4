72805fc3e2ef16e6505cfa385c97cea1d4db5f50
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-19 15:20:20 -0500

    CHanges to image Processing

--- imageProcessing.java
+++ imageProcessing.java
@@ -2,11 +2,26 @@
 
 package week4Assignments;
 
-public class imageProcessing {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+private imageProcessing flipHorizontal(GImage image) {
+	
+	int[][] array = image.getPixelArray();
+	
+	int width = array[0].length;
+	
+	int height = array.length;
+	
+	
+	for (int row = 0; row < height; row++) {
+		
+		for (int p1 = 0; p1 < width / 2; p1++) {
+			
+			int p2 = width -p1 -1;
+			
+			int temp = array[row][p1];array[row][p1] = array[row][p2];array[row][p2] = temp;
+			
+		}
+		
 	}
-
-}
+	
+	return new GImage(array);}
