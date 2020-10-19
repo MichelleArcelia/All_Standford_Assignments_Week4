f83d18a5aee9cce1d566d99c1d66585772c6f8b4
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-19 15:11:18 -0500

    Update uniqueNames.java

--- uniqueNames.java
+++ uniqueNames.java
@@ -2,11 +2,47 @@
 
 package week4Assignments;
 
-public class uniqueNames {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//import acm.program.*; 
+import java.util.*; 
+		
+		public class UniqueNames extends ConsoleProgram {         
+			
+			public void run() {       
+				
+				ArrayList<String> list = new ArrayList<String>();      
+				
+				while (true) {          
+					
+					String name = readLine("NAME HERE: ");          
+					
+					if (name.equals("")) break;          
+					
+					if (!list.contains(name)) {             
+						
+						list.add(name);          
+						
+					}       
+					
+				}       
+				
+				System.out.println("LIST OF UNIQUE NAMES = ");       
+				
+				printList(list);    
+				
+			}    
+			
+			
+			/* Prints out contents of ArrayList, one element per line */    
+			
+			
+			
+			private void printList(ArrayList list) {       
+				
+				for(int i = 0; i < list.size(); i++) {          
+					
+					System.out.println(list.get(i));       
+					
+				}    
 	}
 
 }
